3e465dd6ff84858aa1b6f0be46c65d27e6d54c96
Soumojit Ghosh <ghoshsoumojit53@gmail.com>
2020-06-19 18:34:29 +0530

    Boiler Plate

--- src/components/header.js
+++ src/components/header.js
@@ -2,26 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = props => (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-light static-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark static-top">
+        <div className="container">
+            <a className="navbar-brand" href="#">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
                         <NavLink className = {"nav-link"} to = "/">Home</NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink className = {"nav-link"} to = "/blogs">Blogs</NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink className = {"nav-link"} to = "/forum">Forum</NavLink>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact Us</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact Us</a>
                     </li>
                 </ul>
             </div>
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,8 +2,18 @@ import React from "react";
 
 
 const Homepage = props => (
-    <div>
-        This is the Homepage
+    <div className = "container-fluid">
+        <div className = "title">
+            
+        </div>
+
+        <div className = "about_us">
+            This is the about us
+        </div>
+
+        <div className = "our_services">
+            This  is the services
+        </div>
     </div>
 );
 
